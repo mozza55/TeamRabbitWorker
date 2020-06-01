2a2a0e78a9b558207888939cff60e8fdcf9629cd
mozza55 <mozzarella1103@gmail.com>
2020-06-01 14:54:16 +0900

    [done] multiple Worker 스레드로 구현

--- src/main/java/handler/MessageHandler.java
+++ src/main/java/handler/MessageHandler.java
@@ -23,7 +23,7 @@ public class MessageHandler extends SimpleChannelInboundHandler<NettyMessage> {
     }
 
     protected void channelRead0(ChannelHandlerContext ctx, NettyMessage msg) throws Exception {
-        System.out.println("worker["+workerNum+"("+taskType+")] read : (task/id) "+ msg.getTaskType()+"/"+msg.getBody());
+        System.out.println("worker["+workerNum+"("+taskType+")] read : "+msg.getBody());
 
         //임의 작업 처리 구간
         //Thread.sleep((workerNum%5)*1000);
@@ -31,8 +31,10 @@ public class MessageHandler extends SimpleChannelInboundHandler<NettyMessage> {
         int s = random.nextInt(10);
         Thread.sleep(s*1000);
 
-        String body = "worker"+workerNum+" (task/id)"+ msg.getTaskType()+"/"+msg.getBody()+"  작업 완료";
-        NettyMessage nettyMessage = new NettyMessage((byte)0, (byte)taskType, body.getBytes().length, body);
+        //String body = "worker"+workerNum+" (task/id)"+ msg.getTaskType()+"/"+msg.getBody()+"  작업 완료";
+        //NettyMessage nettyMessage = new NettyMessage((byte)0, (byte)taskType, body.getBytes().length, body);
+
+        NettyMessage nettyMessage = new NettyMessage((byte)0, (byte)taskType, msg.toString().getBytes().length, msg.toString());
         ctx.writeAndFlush(nettyMessage);
 
         System.out.println("worker["+workerNum+"] read : complete :"+ msg.getBody());
